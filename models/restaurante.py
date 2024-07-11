174a723fc6fdfc7790863d01f395491d747f105c
JuliaHown <juliahown87@gmail.com>
2024-07-11 12:27:39 -0300

    atributos privados e property

--- models/restaurante.py
+++ models/restaurante.py
@@ -4,22 +4,27 @@ class Restaurante:
 
     # Método Construtor
     def __init__(self, nome, categoria):
-        self.nome = nome
-        self.categoria = categoria
-        self.ativo = False
+        self._nome = nome.title()
+        self._categoria = categoria.upper()
+        self._ativo = False
         Restaurante.restaurantes.append(self) #Todo objeto criado vai ser inserido na lista
 
     # Método para exibir o objeto em formato de texto
     def __str__(self):
-        return f'{self.nome} | {self.categoria}'
+        return f'{self._nome} | {self._categoria}'
     
     # Método criado para listar
     def listar_restaurantes():
+        print(f"{'Nome do restaurante'.ljust(25)} | {'Categoria'.ljust(25)} | {'Status'}")
         for restaurante in Restaurante.restaurantes:
-            print(f'{restaurante.nome} | {restaurante.categoria} | {restaurante.ativo}')
+            print(f'{restaurante.nome.ljust(25)} | {restaurante.categoria.ljust(25)} | {restaurante.ativo}')
 
+    # Verifica o status do restaurante
+    @property
+    def ativo(self):
+        return 'Ativo ✅' if self._ativo else 'Não ativo❌'
 
-restaurante_applebee = Restaurante('Applebee','Gourmet')
-restaurante_outback = Restaurante('Australiana','Steakhouse')
+restaurante_applebee = Restaurante('applebee','Gourmet')
+restaurante_outback = Restaurante('outblack','Steakhouse')
 
 Restaurante.listar_restaurantes()
