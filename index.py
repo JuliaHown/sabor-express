f19c097689aadbd5987190d15ebccad2aebadccd
JuliaHown <juliahown87@gmail.com>
2024-07-11 13:03:27 -0300

    avaliações adicionadas e método para calcular média

--- index.py
+++ index.py
@@ -1,9 +1,12 @@
 from models.restaurante import Restaurante
 
-restaurante_applebee = Restaurante('applebee','Gourmet')
 restaurante_outback = Restaurante('outblack','Steakhouse')
 
-restaurante_applebee.alterar_status()
+restaurante_outback.alterar_status()
+
+restaurante_outback.receber_avaliacao('Julia', 9)
+restaurante_outback.receber_avaliacao('Guilherme', 8)
+restaurante_outback.receber_avaliacao('Leticia', 7)
 
 def main():
     Restaurante.listar_restaurantes()
--- models/restaurante.py
+++ models/restaurante.py
@@ -1,3 +1,6 @@
+from models.avaliacao import Avaliacao
+
+
 class Restaurante:
 
     restaurantes = []
@@ -7,6 +10,7 @@ class Restaurante:
         self._nome = nome.title()
         self._categoria = categoria.upper()
         self._ativo = False
+        self._avaliacao = []
         Restaurante.restaurantes.append(self) #Todo objeto criado vai ser inserido na lista
 
     # Método para exibir o objeto em formato de texto
@@ -20,12 +24,28 @@ class Restaurante:
         for restaurante in cls.restaurantes:
             print(f'{restaurante._nome.ljust(25)} | {restaurante._categoria.ljust(25)} | {restaurante.ativo}')
 
-    # Método para alterar o status do restaurante
-    def alterar_status(self):
-        self._ativo = not self._ativo
-
     # Verifica o status do restaurante
     @property
     def ativo(self):
         return 'Ativo ✅' if self._ativo else 'Não ativo❌'
+    
+    # Método para alterar o status do restaurante
+    def alterar_status(self):
+        self._ativo = not self._ativo
+    
+    # Método para adicionar uma avaliação ao restaurante
+    def receber_avaliacao(self, cliente, nota):
+        avaliacao = Avaliacao(cliente, nota)
+        self._avaliacao.append(avaliacao)
+
+    # Método para calcular a media de avaliações do restaurante
+    @property
+    def media_avaliacoes(self):
+        if not self.receber_avaliacao:
+            return 0
+        soma_das_notas = sum(avaliacao._nota for avaliacao in self._avaliacao)
+        quantidade_notas = len(self._avaliacao)
+        media = round(soma_das_notas / quantidade_notas, 1)
+        return media
+
 
